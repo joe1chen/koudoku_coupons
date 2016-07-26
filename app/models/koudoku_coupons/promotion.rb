22c1d65840fec68b83531776c5c6990ca3313582
Joseph Chen <joseph.chen@gmail.com>
2016-07-26 12:17:34 -0700

    Mongoid model

--- app/models/koudoku_coupons/promotion.rb
+++ app/models/koudoku_coupons/promotion.rb
@@ -1,5 +1,12 @@
 module KoudokuCoupons
-  class Promotion < ActiveRecord::Base
+  class Promotion
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    field :coupon_code, type: String
+    field :name, type: String
+    field :redirect, type: String
+
     attr_accessible :coupon_code, :name, :redirect
   end
 end
